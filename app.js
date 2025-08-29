e10dddaf1cfd32d32164a0e78d85983f8825d6c2
Maaz Usmani <maaz.usmani@squareboat.com>
2025-08-29 18:11:00 +0530

    Update .gitignore to include .env file and modify server listen logic to use environment variable for port

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ io.on("connection", (socket) => {
     console.log("Client disconnected");
   });
 });
-
-server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
